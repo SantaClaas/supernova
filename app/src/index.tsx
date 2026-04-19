39d71a1f6df7cedd3b2bbfbc231bfcd0b3a6c0ae
Claas <claas@claas.dev>
2026-04-19 13:16:57 +0200

    Test out Temporal API

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,6 +3,12 @@ import { render } from 'solid-js/web'
 import './index.css'
 import App from './App.tsx'
 
-const root = document.getElementById('root')
+// If I am not mistaken, this should keep the polyfill out of the bundle
+if (!("Temporal" in globalThis)) {
+    console.debug("Temporal not found, polyfilling...");
+    const { Temporal } = await import("temporal-polyfill");
+    // @ts-expect-error
+    globalThis.Temporal = Temporal;
+}
 
-render(() => <App />, root!)
+render(() => <App />, document.body)
