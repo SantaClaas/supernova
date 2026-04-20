13849c64df601100b8e075d3ad8f505f769ad611
Claas <claas@claas.dev>
2026-04-21 00:41:04 +0200

    Save game

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,28 +1,94 @@
+import { createMemo, For, Show } from "solid-js";
+import { createStore } from "solid-js/store";
+
 function App() {
-  const durations = [
-    Temporal.Duration.from({ minutes: 40 }),
-    Temporal.Duration.from({ hours: 4 }),
-    Temporal.Duration.from({ hours: 1, minutes: 10 }),
-    Temporal.Duration.from({ hours: 3, minutes: 30 }),
-    Temporal.Duration.from({ hours: 1, minutes: 30 }),
-    Temporal.Duration.from({ hours: 0, minutes: 30 }),
-    Temporal.Duration.from({ hours: 5, minutes: 10 }),
-    Temporal.Duration.from({ hours: 3, minutes: 45 }),
-    Temporal.Duration.from({ hours: 0, minutes: 30 }),
-    Temporal.Duration.from({ hours: 1, minutes: 0 }),
-    Temporal.Duration.from({ hours: 8, minutes: 15 }),
-  ];
+  const [store, setStore] = createStore<
+    [start: Temporal.PlainDateTime, end: Temporal.PlainDateTime][]
+  >([]);
+
+  function handleSubmit(event: SubmitEvent) {
+    event.preventDefault();
+
+    if (!(event.target instanceof HTMLFormElement)) throw new Error("Expected target to be a form");
+
+    const startInput = event.target.elements.namedItem("start");
+    if (!(startInput instanceof HTMLInputElement))
+      throw new Error("Expected start input to be an input");
+
+    const start = Temporal.PlainDateTime.from(startInput.value);
+
+    if (start === undefined) throw new Error("Expected start to be a valid date");
+
+    const endInput = event.target.elements.namedItem("end");
+    if (!(endInput instanceof HTMLInputElement))
+      throw new Error("Expected end input to be an input");
+
+    const end = Temporal.PlainDateTime.from(endInput.value);
+    if (end === undefined) throw new Error("Expected end to be a valid date");
+
+    setStore(store.length, [start, end]);
+  }
+
+  const now = Temporal.Now.plainDateTimeISO();
+  const start = now.toString().substring(0, 16);
+  const end = now.add({ minutes: 30 }).toString().substring(0, 16);
+  const rows = createMemo(() =>
+    store.map(([start, end]) => ({
+      year: start.toLocaleString(undefined, { year: "numeric" }),
+      month: start.toLocaleString(undefined, { month: "short" }),
+      week: start.weekOfYear?.toString(),
+      day: start.toLocaleString(undefined, { day: "numeric" }),
+      dayOfWeek: start.toLocaleString(undefined, { weekday: "long" }),
+      start: start.toLocaleString(undefined, { timeStyle: "medium" }),
+      end: end.toLocaleString(undefined, { timeStyle: "medium" }),
+    })),
+  );
   return (
     <>
-      <p>
-        Duration{" "}
-        {durations
-          .reduce(
-            (previous, current) => previous.add(current),
-            new Temporal.Duration(),
-          )
-          .toString()}
-      </p>
+      <h1>Time</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Month</th>
+            <th>Week</th>
+            <th>Day</th>
+            <th>Start</th>
+            <th>End</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Show
+            when={rows().length > 0}
+            fallback={
+              <tr>
+                <td colSpan="5">No data</td>
+              </tr>
+            }
+          >
+            <For each={rows()}>
+              {(row) => (
+                <tr>
+                  <td>{row.year}</td>
+                  <td>{row.month}</td>
+                  <td>{row.week}</td>
+                  <td>
+                    {row.dayOfWeek} {row.day}
+                  </td>
+                  <td>{row.start}</td>
+                  <td>{row.end}</td>
+                </tr>
+              )}
+            </For>
+          </Show>
+        </tbody>
+      </table>
+      <form onSubmit={handleSubmit}>
+        <label for="start">Start </label>
+        <input type="datetime-local" id="start" name="start" value={start} />
+        <label for="end">End</label>
+        <input type="datetime-local" id="end" name="end" value={end} />
+        <button type="submit">Save</button>
+      </form>
     </>
   );
 }
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,14 +1,16 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
-import './index.css'
-import App from './App.tsx'
+import { render } from "solid-js/web";
+import "./index.css";
+import App from "./App.tsx";
 
 // Keep polyfill out of the bundle. You can see this as vite generates two js bundles, one with only the polyfill and one without.
 if (!("Temporal" in globalThis)) {
-    console.debug("Temporal not found, polyfilling...");
-    const { Temporal } = await import("temporal-polyfill");
-    // @ts-expect-error
-    globalThis.Temporal = Temporal;
+  console.debug("Temporal not found, polyfilling...");
+  const { Temporal, toTemporalInstant } = await import("temporal-polyfill");
+  // @ts-expect-error
+  globalThis.Temporal = Temporal;
+  // @ts-expect-error
+  Date.prototype.toTemporalInstant = toTemporalInstant;
 }
 
-render(() => <App />, document.body)
+render(() => <App />, document.body);
